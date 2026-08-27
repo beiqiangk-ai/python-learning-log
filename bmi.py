7b9a609ef1c7021f137fa26998bf319bb4a8b5ee
beiqiangk-ai <beiqiangk@gmail.com>
2026-08-28 06:13:25 +0800

    Add files via upload

--- bmi.py
+++ bmi.py
@@ -1,4 +1,12 @@
 height = float(input("请输入你的身高(米,比如1.70):"))
 weight = float(input("请输入你的体重(公斤,比如65):"))
 bmi = weight / (height * height)
-print("你的BMI是:",bmi)
+print("你的BMI是: %.2f" % bmi)
+if bmi < 18.5:
+    print("体重状态: 偏瘦")
+elif bmi < 24:
+    print("体重状态: 正常")
+elif bmi < 28:
+    print("体重状态: 偏胖")
+else:
+    print("体重状态: 肥胖")
